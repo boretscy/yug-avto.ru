21366cbcfc79b74b54ac675fdf09ab5bb2f87f18
boretscy <boretscy@yug-avto.ru>
2026-09-11 10:21:35 +0000

    feat(snippets): add blog visual editor snippets to local theme template

---
+++ local/templates/yugavto.theme.2025/snippets/.content.php
@@ -0,0 +1,7 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
+<?
+$SNIPPETS = Array();
+$SNIPPETS['snippet0001.snp'] = Array('title' => 'Важно', 'description' => 'Важно');
+$SNIPPETS['snippet0002.snp'] = Array('title' => 'Цитата', 'description' => 'Цитата');
+$SNIPPETS['snippet0003.snp'] = Array('title' => 'Итоговый вывод');
+?>
